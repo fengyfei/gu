501913be71813fec22fab5acdd33548b7c7ce621
Feng Yifei <fengyfei@gmail.com>
2017-12-21 11:14:11 +0800

    nats connection

---
+++ libs/natstreaming/connection.go
@@ -0,0 +1,73 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017 SmartestEE Co., Ltd.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/*
+ * Revision History:
+ *     Initial: 2017/12/21        Feng Yifei
+ */
+
+package natstreaming
+
+import (
+	stan "github.com/nats-io/go-nats-streaming"
+)
+
+// Connection is a connection to nats streaming server.
+type Connection struct {
+	ClusterID string // Should be same with the nats streaming server
+	ClientID  string
+	URL       string
+	conn      stan.Conn
+}
+
+// NewConnection creates a Connection to a nats streaming server.
+func NewConnection(clusterID, clientID, url string) (*Connection, error) {
+	c := &Connection{
+		ClusterID: clusterID,
+		ClientID:  clientID,
+		URL:       url,
+	}
+
+	if err := c.connect(); err != nil {
+		return nil, err
+	}
+
+	return c, nil
+}
+
+func (c *Connection) connect() error {
+	conn, err := stan.Connect(c.ClusterID, c.ClientID, stan.NatsURL(c.URL))
+	if err != nil {
+		return err
+	}
+
+	c.conn = conn
+
+	return nil
+}
+
+// Close tear down the connection.
+func (c *Connection) Close() error {
+	return c.conn.Close()
+}
