64c933d9968121db0ea14dc323fb2b6031cdfb7e
LLLeon <jiachenhui1989@gmail.com>
2017-12-26 19:28:13 +0800

    Add JWT example #77

--- libs/http/server/examples/http.go
+++ libs/http/server/examples/http.go
@@ -30,15 +30,56 @@
 package main
 
 import (
+	"time"
+
+	jwtgo "github.com/dgrijalva/jwt-go"
+
 	"github.com/fengyfei/gu/libs/constants"
 	"github.com/fengyfei/gu/libs/http/server"
 	"github.com/fengyfei/gu/libs/http/server/middleware"
 	"github.com/fengyfei/gu/libs/logger"
 )
 
-type echo struct {
-	Message *string `json:"message" validate:"required,min=6"`
+const (
+	tokenExpireInHour = 48
+
+	claimsKey    = "user"
+	claimUID     = "uid"
+	claimExpire  = "exp"
+	respTokenKey = "token"
+
+	invalidUID int32 = -1
+)
+
+var (
+	tokenHMACKey        = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9"
+	defaultUserID int32 = 2
+	URLMap        map[string]struct{}
+
+	jwtConfig = middleware.JWTConfig{
+		Skipper:    customSkipper,
+		SigningKey: []byte(tokenHMACKey),
+		// use to extract claims from context
+		ContextKey: claimsKey,
+	}
+)
+
+func init() {
+	URLMap = make(map[string]struct{})
+
+	URLMap["/login"] = struct{}{}
 }
+
+type (
+	loginReq struct {
+		Name *string `json:"name" validate:"required,alphanum,min=2,max=30"`
+		Pwd  *string `json:"pwd" validate:"required,printascii,excludesall=@-,min=6,max=30"`
+	}
+
+	echo struct {
+		Message *string `json:"message" validate:"required,min=6"`
+	}
+)
 
 func echoHandler(c *server.Context) error {
 	var (
@@ -86,36 +127,110 @@ func panicHandler(c *server.Context) error {
 
 func loginFilter(c *server.Context) bool {
 	if c.Request().RequestURI != "/login" {
-		// there must be this one
-		c.ServeJSON(map[string]interface{}{constants.RespKeyStatus: constants.ErrPermission})
-		return false
+		claims := c.Request().Context().Value(claimsKey)
+		uid := GetUserID(claims)
+
+		if uid != defaultUserID {
+			// must call ServeJSON
+			c.ServeJSON(map[string]interface{}{constants.RespKeyStatus: constants.ErrPermission})
+			return false
+		}
 	}
 
 	return true
 }
 
-var (
-	defaultJWT = middleware.JWTWithKey("uid")
-)
+func login(c *server.Context) error {
+	var (
+		err error
+		req loginReq
+	)
+
+	if err = c.JSONBody(&req); err != nil {
+		logger.Error(err)
+		return c.ServeJSON(map[string]interface{}{constants.RespKeyStatus: constants.ErrInvalidParam})
+	}
+
+	_, token, err := NewToken(defaultUserID)
+	if err != nil {
+		logger.Error(err)
+		return c.ServeJSON(map[string]interface{}{constants.RespKeyStatus: constants.ErrPermission})
+	}
+
+	logger.Debug("token:", token)
+	return c.ServeJSON(map[string]interface{}{
+		constants.RespKeyStatus: constants.ErrSucceed,
+		constants.RespKeyToken:  token,
+	})
+}
+
+func verifyJWT(c *server.Context) error {
+	claims := c.Request().Context().Value(claimsKey)
+	uid := GetUserID(claims)
+
+	if uid != defaultUserID {
+		logger.Debug("JWT verify failure...")
+		return c.ServeJSON(map[string]interface{}{constants.RespKeyStatus: constants.ErrPermission})
+	}
+
+	logger.Debug("JWT verify success!")
+	return c.ServeJSON(map[string]interface{}{constants.RespKeyStatus: constants.ErrSucceed})
+}
+
+// GetUserID extract uid from context value
+func GetUserID(v interface{}) int32 {
+	claims := v.(jwtgo.MapClaims)
+
+	if id, ok := claims[claimUID].(float64); ok {
+		return int32(id)
+	}
+
+	return invalidUID
+}
+
+// NewToken generates a JWT token.
+func NewToken(uid int32) (string, string, error) {
+	token := jwtgo.New(jwtgo.SigningMethodHS256)
+
+	claims := token.Claims.(jwtgo.MapClaims)
+	claims[claimUID] = uid
+	claims[claimExpire] = time.Now().Add(time.Hour * tokenExpireInHour).Unix()
+
+	t, err := token.SignedString([]byte(tokenHMACKey))
+	return respTokenKey, t, err
+}
+
+func customSkipper(c *server.Context) bool {
+	if _, ok := URLMap[c.Request().RequestURI]; ok {
+		return true
+	}
+
+	return false
+}
 
 func main() {
 	configuration := &server.Configuration{
 		Address: "0.0.0.0:9573",
 	}
 
+	// routers
 	router := server.NewRouter()
 	router.Post("/", echoHandler, loginFilter)
 	router.Post("/post", postHandler, loginFilter)
 	router.Get("/panic", panicHandler, loginFilter)
-
-	ep := server.NewEntrypoint(configuration, nil)
-	cors := middleware.CORSAllowAll()
+	router.Post("/login", login)
+	router.Post("/verify", verifyJWT, loginFilter)
 
 	// add middlewares
+	corsMiddleware := middleware.CORSAllowAll()
+	jwtMiddleware := middleware.JWTWithConfig(jwtConfig)
+
+	ep := server.NewEntrypoint(configuration, nil)
+
 	ep.AttachMiddleware(middleware.NegroniRecoverHandler())
 	ep.AttachMiddleware(middleware.NegroniLoggerHandler())
-	ep.AttachMiddleware(cors)
-	ep.AttachMiddleware(defaultJWT)
+	ep.AttachMiddleware(corsMiddleware)
+	ep.AttachMiddleware(jwtMiddleware)
 
 	if err := ep.Start(router.Handler()); err != nil {
 		logger.Error(err)
