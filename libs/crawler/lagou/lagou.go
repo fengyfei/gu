56185b2fbe7ee011a4b9000f6097f38918bd3334
shitime <1257201113@qq.com>
2018-01-25 17:52:47 +0800

    modify lagou

---
+++ libs/crawler/lagou/lagou.go
@@ -0,0 +1,237 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2018 SmartestEE Co., Ltd..
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/*
+ * Revision History:
+ *     Initial: 2018/01/24        Shi Ruitao
+ */
+
+package lagou
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"strings"
+
+	"github.com/fengyfei/gu/libs/crawler"
+)
+
+var DataPipe chan LagouResult = make(chan LagouResult)
+
+const (
+	jobHtml = "https://www.lagou.com/jobs/%d.html"
+
+	Method     = "POST"
+	RequestUrl = "https://www.lagou.com/jobs/positionAjax.json?needAddtionalResult=false&isSchoolJob=0"
+	Referer    = "https://www.lagou.com/jobs/list_java?city=%E5%85%A8%E5%9B%BD&cl=false&fromSearch=true&labelWords=&suginput="
+	Cookie     = "JSESSIONID=ABAAABAACDBABJBF293E69FB60C42BE49201229C99D8FCD; user_trace_token=20171205104807-b96aac92-d966-11e7-9c13-5254005c3644; LGUID=20171205104807-b96aaf6b-d966-11e7-9c13-5254005c3644; index_location_city=%E5%85%A8%E5%9B%BD; _putrc=68D38A1FF9900849; login=true; unick=%E6%97%B6%E7%91%9E%E6%B6%9B; _ga=GA1.2.206867280.1512442088; _gid=GA1.2.136452891.1514977679; Hm_lvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1512442089,1512443109,1514977679; LGSID=20180104095511-4cb7a44f-f0f2-11e7-bbf1-525400f775ce; _gat=1; TG-TRACK-CODE=index_search; Hm_lpvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1515033736; LGRID=20180104104216-e02d395f-f0f8-11e7-bc05-525400f775ce; SEARCH_ID=5a8f1728749c46b299033be73e954568"
+	UserAgent  = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36"
+)
+
+type lagouObject struct {
+	Content lagouContent `json:"content"`
+}
+
+type lagouContent struct {
+	PositionResult lagouPositionResult `json:"positionResult"`
+}
+
+type lagouPositionResult struct {
+	Result []LagouResult `json:"result"`
+}
+
+// LagouResult used to store the  data.
+type LagouResult struct {
+	CompanyId             int         `json:"companyId"`
+	CompanyShortName      string      `json:"companyShortName"`
+	CreateTime            string      `json:"createTime"`
+	PositionId            int         `json:"positionId"`
+	Score                 int         `json:"score"`
+	PositionAdvantage     string      `json:"positionAdvantage"`
+	Salary                string      `json:"salary"`
+	WorkYear              string      `json:"workYear"`
+	Education             string      `json:"education"`
+	City                  string      `json:"city"`
+	PositionName          string      `json:"positionName"`
+	CompanyLogo           string      `json:"companyLogo"`
+	FinanceStage          string      `json:"financeStage"`
+	IndustryField         string      `json:"industryField"`
+	JobNature             string      `json:"jobNature"`
+	CompanySize           string      `json:"companySize"`
+	Approve               int         `json:"approve"`
+	CompanyLabelList      []string    `json:"companyLabelList"`
+	PublisherId           int         `json:"publisherId"`
+	District              interface{} `json:"district"`
+	PositionLabels        []string    `json:"positionLables"`
+	IndustryLabels        []string    `json:"industryLables"`
+	BusinessZones         interface{} `json:"businessZones"`
+	AdWord                int         `json:"adWord"`
+	Longitude             string      `json:"longitude"`
+	Latitude              string      `json:"latitude"`
+	ImState               string      `json:"imState"`
+	LastLogin             uint64      `json:"lastLogin"`
+	Explain               interface{} `json:"explain"`
+	Plus                  interface{} `json:"plus"`
+	PcShow                int         `json:"pcShow"`
+	AppShow               int         `json:"appShow"`
+	Deliver               int         `json:"deliver"`
+	GradeDescription      interface{} `json:"gradeDescription"`
+	PromotionScoreExplain interface{} `json:"promotionScoreExplain"`
+	FirstType             string      `json:"firstType"`
+	SecondType            string      `json:"secondType"`
+	IsSchoolJob           int         `json:"isSchoolJob"`
+	SubwayLine            string      `json:"subwayline"`
+	StationName           string      `json:"stationname"`
+	LineStation           string      `json:"linestaion"`
+	FormatCreateTime      string      `json:"formatCreateTime"`
+	CompanyFullName       string      `json:"companyFullName"`
+}
+
+type lagouClient struct {
+	job *string
+}
+
+// NewLaGouCrawler generates a crawler for lagou.
+func NewLaGouCrawler(job string) crawler.Crawler {
+	return &lagouClient{
+		job: &job,
+	}
+}
+
+// Crawler interface Init
+func (lc *lagouClient) Init() error {
+	return nil
+}
+
+// Start interface Start
+func (lc *lagouClient) Start() error {
+	var sum int
+	for i := 1; true; i++ {
+		urls := lc.getJobHttp(i)
+		if len(urls) == 0 {
+			break
+		}
+		sum += len(urls)
+	}
+	fmt.Println("sum:", sum)
+	return nil
+}
+
+func (lc *lagouClient) getJobHttp(pn int) []string {
+	hc := newClient()
+
+	data := url.Values{}
+	data.Set("fires", "fasle")
+	data.Set("pn", fmt.Sprintf("%d", pn))
+	data.Set("kd", *lc.job)
+	b := strings.NewReader(data.Encode())
+
+	req := newRequest(Method, RequestUrl, b)
+	lc.setHeader(req, Referer, Cookie, RequestUrl)
+
+	resp, err := hc.Do(req)
+	if err != nil {
+		log.Fatalln("error in doing a http request.", err)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln("error in reading response body.", err)
+	}
+
+	var object lagouObject
+
+	err = json.Unmarshal(body, &object)
+	if err != nil {
+		log.Fatalln("error in unmarshalling response body.", err)
+	}
+
+	var urls = make([]string, 0)
+	for _, value := range object.Content.PositionResult.Result {
+		urls = append(urls, fmt.Sprintf(jobHtml, value.PositionId))
+	}
+
+	go chRoutine()
+
+	for i := 0; i < len(urls); i++ {
+		DataPipe <- object.Content.PositionResult.Result[i]
+	}
+	return urls
+}
+
+func chRoutine() {
+	for {
+		fmt.Println(<-DataPipe)
+	}
+}
+
+func newClient() *http.Client {
+	return &http.Client{}
+}
+
+func newRequest(method, url string, body io.Reader) *http.Request {
+	req, err := http.NewRequest(method, url, body)
+	if err != nil {
+		log.Fatalln("error in creating a new request.", err)
+	}
+	return req
+}
+
+func (lc *lagouClient) setHeader(req *http.Request, referer, cookie, user_agent string) {
+	var headers = make(map[string]string)
+
+	for k, v := range defaultHeader() {
+		headers[k] = v
+	}
+
+	headers["Referer"] = referer
+	headers["Cookie"] = cookie
+	headers["User-Agent"] = user_agent
+
+	header(req, headers)
+}
+
+func defaultHeader() map[string]string {
+	var defaultHeaders = make(map[string]string)
+
+	defaultHeaders["Accept"] = "application/json, text/javascript, */*; q=0.01"
+	defaultHeaders["Accept-Encoding"] = "gzip, deflate, br"
+	defaultHeaders["Accept-Language"] = "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7"
+	defaultHeaders["Connection"] = "keep-alive"
+	defaultHeaders["Content-Type"] = "application/x-www-form-urlencoded; charset=UTF-8"
+	defaultHeaders["User-Agent"] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36"
+
+	return defaultHeaders
+}
+
+func header(req *http.Request, headers map[string]string) {
+	for key, value := range headers {
+		req.Header.Set(key, value)
+	}
+}
