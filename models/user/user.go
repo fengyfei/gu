afd6678065a8f3ee3e3abee39953b1ec3ad2dd65
shitime <1257201113@qq.com>
2018-03-02 00:21:14 +0800

    modify user

--- models/user/user.go
+++ models/user/user.go
@@ -25,6 +25,7 @@
 /*
  * Revision History:
  *     Initial: 2018/01/21        Chen Yanchen
+ *	   Modify:  2018/02/01        Shi Ruitao
  */
 
 package user
@@ -39,9 +40,8 @@ import (
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 
-	"github.com/fengyfei/gu/applications/bbs/conf"
-	"github.com/fengyfei/gu/applications/bbs/initialize"
 	"github.com/fengyfei/gu/libs/mongo"
+	"github.com/fengyfei/gu/libs/orm"
 	"github.com/fengyfei/gu/libs/security"
 )
 
@@ -55,42 +55,91 @@ const (
 	Mobile
 )
 
-// PhoneRegister
-type PhoneRegister struct {
-	Phone    string `json:"phone" validate:"required,alphanum,len=11"`
-	Password string `json:"password" validate:"required,min=6,max=30"`
-	Name     string `json:"name" validate:"required,alphaunicode,min=2,max=30"`
-}
-
-// PhoneLogin
-type PhoneLogin struct {
-	Phone    string `json:"phone" validate:"required,alphanum,len=11"`
-	Password string `json:"password" validate:"required,min=6,max=30"`
-}
-
-// ChangePass
-type ChangePass struct {
-	OldPass string `json:"oldPass" validate:"required,min=6,max=30"`
-	NewPass string `json:"newPass" validate:"required,min=6,max=30"`
-}
-
 var (
 	// UserServer
 	UserServer *UserServiceProvider
 	session    *mongo.Connection
-	// DefaultImage
-	DefaultImage = ""
-	// ErrInvalidUser
-	ErrInvalidUser = errors.New("User doesn't exists.")
+
 	// ErrInvalidPass
 	ErrInvalidPass = errors.New("the password error.")
-	// ErrUserExists
-	ErrUserExists = errors.New("User already exists.")
 )
+
+type (
+	PhoneRegister struct {
+		Phone    string `json:"phone" validate:"required,alphanum,len=11"`
+		Password string `json:"password" validate:"required,min=6,max=30"`
+		UserName string `json:"username" validate:"required,alphaunicode,min=2,max=30"`
+	}
+
+	PhoneLogin struct {
+		Phone    string `json:"phone" validate:"required,alphanum,len=11"`
+		Password string `json:"password" validate:"required,min=6,max=30"`
+	}
+
+	ChangePass struct {
+		OldPass string `json:"oldPass" validate:"required,min=6,max=30"`
+		NewPass string `json:"newPass" validate:"required,min=6,max=30"`
+	}
+
+	WechatLogin struct {
+		//UserName   string `json:"username"`
+		//Sex        uint8  `json:"sex"`
+		UnionID    string `json:"unionid"`
+		SessionKey string `json:"session_key"`
+	}
+
+	WechatCode struct {
+		UserName string `json:"username"`
+		Sex      uint8  `json:"sex"`
+		Phone    string `json:"phone" validate:"required,alphanum,len=11"`
+		Code     string `json:"code" validate:"required"`
+	}
+
+	WechatData struct {
+		SessionKey string `json:"session_key"`
+		UnionID    string `json:"unionid"`
+	}
+
+	UserData struct {
+		Token    string `json:"token"`
+		UserName string `json:"username"`
+		Phone    string `json:"phone"`
+		Avatar   string `json:"avatar"`
+		Sex      uint8  `json:"sex"`
+	}
+
+	ChangeInfo struct {
+		UserName string `json:"username"`
+		Sex      uint8  `json:"sex"`
+		Avatar   string `json:"avatar"`
+	}
+	Avatar struct {
+		AvatarID bson.ObjectId `bson:"_id,omitempty"`
+		UserID   uint32        `bson:"userid,omitempty" json:"id"`
+		Avatar   string        `bson:"avatar" json:"avatar"`
+	}
+)
+
+// User represents users information
+type User struct {
+	UserID   uint32 `gorm:"column:id;primary_key;auto_increment" json:"userID"`
+	UserName string `gorm:"column:username;size:16"`
+	Sex      uint8  `gorm:"column:sex"`
+	Password string `gorm:"column:password;type:varchar(128)" json:"password" validate:"required,alphanum,min=6,max=30"`
+	Phone    string `gorm:"type:varchar(16)" json:"phone" validate:"required,numeric,len=11"`
+	UnionID  string `gorm:"column:unionid;type:varchar(128)"`
+	//Avatar    string    `gorm:"column:avatar;type:varchar(128)"`
+	Created   time.Time `gorm:"column:created"`
+	LastLogin time.Time `gorm:"column:lastlogin"`
+	IsAdmin   bool      `gorm:"column:isadmin"`
+	Type      int       `grom:"column:type"`
+	IsActive  bool      `gorm:"column:isactive;not null;default:1"`
+	//ArticleNum int64     `gorm:"column:articlenum;not null;default:0"`
+}
 
 func init() {
 	const collection = "avatar"
-	url := conf.BBSConfig.MongoURL + "/" + "bbs"
+	url := "mongodb://127.0.0.1:27017"
 	s, err := mgo.Dial(url)
 	if err != nil {
 		panic(err)
@@ -98,23 +147,8 @@ func init() {
 
 	s.SetMode(mgo.Monotonic, true)
 
-	session = mongo.NewConnection(s, "bbs", collection)
+	session = mongo.NewConnection(s, "user", collection)
 	UserServer = &UserServiceProvider{}
-}
-
-// User represents users information
-type User struct {
-	UserID     uint32    `gorm:"column:id;primary_key;auto_increment" json:"userID"`
-	UserName   string    `gorm:"column:UserName;size:16"`
-	Password   string    `gorm:"column:Password;type:varchar(128)" json:"password" validate:"required,alphanum,min=6,max=30"`
-	Phone      string    `gorm:"type:varchar(16)" json:"phone" validate:"required,numeric,len=11"`
-	UnionID    string    `gorm:"column:UnionID;type:varchar(128)"`
-	AvatarID   string    `gorm:"column:AvatarID;type:varchar(128)"`
-	Created    time.Time `gorm:"column:Created"`
-	LastLogin  time.Time `gorm:"column:LastLogin"`
-	Type       int       `grom:"column:Type"`
-	IsActive   bool      `gorm:"column:IsActive;not null;default:1"`
-	ArticleNum int64     `gorm:"column:ArticleNum;not null;default:0"`
 }
 
 // TableName
@@ -122,191 +156,175 @@ func (u User) TableName() string {
 	return "users"
 }
 
-// Avatar
-type Avatar struct {
-	AvatarID bson.ObjectId `bson:"_id,omitempty"`
-	UserID   uint64        `bson:"UserID"`
-	Avatar   string        `bson:"Avatar"`
-}
-
 // WeChatLogin
-func (this *UserServiceProvider) WeChatLogin(username, unionID *string) (uint64, error) {
-	conn, err := initialize.Pool.Get()
-	if err != nil {
-		return 0, err
-	}
+func (this *UserServiceProvider) WeChatLogin(conn orm.Connection, info *WechatLogin) (*User, *Avatar, error) {
+	var err error
+	var user User
 
-	user := &User{}
-	db := conn.(*gorm.DB).Exec("USE user")
-	err = db.Where("unionID = ?", *unionID).First(&user).Error
+	db := conn.(*gorm.DB)
+
+	err = db.Where("unionID = ?", info.UnionID).First(&user).Error
 	if err != nil {
 		if err == gorm.ErrRecordNotFound {
 			r := rand.New(rand.NewSource(time.Now().UnixNano()))
-			s := fmt.Sprintf("%s%s", unionID, r.Intn(10000))
+			s := fmt.Sprintf("%s%s", info.UnionID, r.Intn(10000))
 			salt, err := security.SaltHashGenerate(&s)
 			if err != nil {
-				return 0, err
+				return nil, nil, err
 			}
 
-			user.UserName = *username
 			user.Type = WeChat
 			user.Password = string(salt)
-			user.AvatarID = DefaultImage
 			user.IsActive = true
-			user.UnionID = *unionID
+			user.IsAdmin = false
+			user.UnionID = info.UnionID
 			user.Created = time.Now()
 			user.LastLogin = time.Now()
 
 			err = db.Model(&User{}).Create(&user).Error
 			if err != nil {
-				return 0, err
+				return nil, nil, err
 			}
 
-			err = db.Where("unionID = ?", *unionID).First(&user).Error
-			return user.UserID, nil
+			err = db.Where("unionID = ?", info.UnionID).First(&user).Error
+			if err != nil {
+				return nil, nil, err
+			}
 		}
-		return 0, err
+		return nil, nil, err
 	}
-
-	return user.UserID, nil
+	avatar, err := userAvatar(user)
+	if err != nil {
+		return nil, nil, err
+	}
+	return &user, avatar, nil
 }
 
-// ChangeName
-func (this *UserServiceProvider) ChangeName(userID uint64, newname *string) error {
-	var (
-		err  error
-		user User
-	)
+// Change user information
+func (this *UserServiceProvider) ChangeInfo(conn orm.Connection, id uint32, change *ChangeInfo) error {
+	var user User
 
-	conn, err := initialize.Pool.Get()
+	db := conn.(*gorm.DB)
+
+	err := db.Where("id = ?", id).First(&user).Error
 	if err != nil {
 		return err
 	}
 
-	db := conn.(*gorm.DB).Exec("USE user")
-
-	err = db.Where("id = ?", 1001).Find(&user).Error
-	if err != nil {
-		return err
-	}
-	user.UserName = *newname
-
-	return db.Model(&user).Save(&user).Error
-}
-
-// ChangeAvatar
-func (this *UserServiceProvider) ChangeAvatar(userID uint64, avatar *string) (string, error) {
-	var res Avatar
-	updater := bson.M{"$set": bson.M{
-		"avatar": *avatar,
-	}}
-
-	conn := session.Connect()
-	defer conn.Disconnect()
-
-	err := conn.Update(bson.M{"_id": bson.ObjectId(userID)}, updater)
-	if err != nil {
-		return "", nil
-	}
-
-	query := bson.M{"userID": userID}
-	err = conn.GetUniqueOne(query, &res)
-
-	return res.AvatarID.Hex(), err
-}
-
-// PhoneRegister
-func (this *UserServiceProvider) PhoneRegister(p *PhoneRegister) error {
-	salt, err := security.SaltHashGenerate(&p.Password)
+	c := session.Connect()
+	_, err = c.Upsert(bson.M{"_id": id}, change.Avatar)
 	if err != nil {
 		return err
 	}
 
-	conn, err := initialize.Pool.Get()
-	if err != nil {
-		return err
-	}
+	user.UserName = change.UserName
+	user.Sex = change.Sex
 
-	db := conn.(*gorm.DB).Exec("USE user")
-	user := &User{}
-
-	err = db.Where("Phone = ?", p.Phone).First(&user).Error
-	if err == gorm.ErrRecordNotFound {
-		user.UserName = p.Name
-		user.Type = Mobile
-		user.Phone = p.Phone
-		user.Password = string(salt)
-		user.AvatarID = DefaultImage
-		user.IsActive = true
-		user.Created = time.Now()
-		user.LastLogin = time.Now()
-
-		return db.Model(&User{}).Create(&user).Error
-	}
-
-	return ErrUserExists
-}
-
-// PhoneLogin
-func (this *UserServiceProvider) PhoneLogin(p *PhoneLogin) (uint64, error) {
-	conn, err := initialize.Pool.Get()
-	if err != nil {
-		return 0, err
-	}
-
-	db := conn.(*gorm.DB).Exec("USE user")
-	user := &User{}
-
-	err = db.Where("Phone = ?", p.Phone).First(&user).Error
-	if err == gorm.ErrRecordNotFound {
-		return 0, ErrInvalidUser
-	}
-
-	if !security.SaltHashCompare([]byte(user.Password), &p.Password) {
-		return 0, ErrInvalidPass
-	}
-
-	return user.UserID, err
-}
-
-// ChangePassword
-func (this *UserServiceProvider) ChangePassword(id uint, oldPass, newPass *string) error {
-	conn, err := initialize.Pool.Get()
-	if err != nil {
-		return err
-	}
-
-	db := conn.(*gorm.DB).Exec("USE user")
-	user := &User{}
-
-	err = db.Where("id = ?", id).First(&user).Error
-	if err == gorm.ErrRecordNotFound {
-		return err
-	}
-
-	if !security.SaltHashCompare([]byte(user.Password), oldPass) {
-		return ErrInvalidPass
-	}
-
-	salt, err := security.SaltHashGenerate(newPass)
-	if err != nil {
-		return err
-	}
-	user.Password = string(salt)
 	return db.Save(&user).Error
 }
 
-// GetUserByID gets user's information by userId.
-func (this *UserServiceProvider) GetUserByID(userId uint64) (*User, error) {
-	conn, err := initialize.Pool.Get()
+// Register by phone
+func (this *UserServiceProvider) PhoneRegister(conn orm.Connection, register *PhoneRegister) error {
+	salt, err := security.SaltHashGenerate(&register.Password)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
+	user := User{
+		UserName:  register.UserName,
+		Phone:     register.Phone,
+		UnionID:   register.Phone,
+		Password:  string(salt),
+		Type:      Mobile,
+		IsAdmin:   false,
+		Created:   time.Now(),
+		LastLogin: time.Now(),
+	}
+
+	db := conn.(*gorm.DB)
+
+	return db.Create(&user).Error
+}
+
+func (this *UserServiceProvider) PhoneLogin(conn orm.Connection, login *PhoneLogin) (*User, *Avatar, error) {
+	var user User
+
+	db := conn.(*gorm.DB)
+
+	err := db.Where("phone = ?", login.Phone).First(&user).Error
+	if err != nil {
+		return nil, nil, err
+	}
+	if !security.SaltHashCompare([]byte(user.Password), &login.Password) {
+		return nil, nil, ErrInvalidPass
+	}
+	avatar, err := userAvatar(user)
+	return &user, avatar, nil
+}
+
+// create and insert avatar for mongodb
+func userAvatar(user User) (*Avatar, error) {
+	var avatar Avatar
+	c := session.Connect()
+	err := c.GetUniqueOne(bson.M{"userid": user.UserID}, &avatar)
+
+	if err != nil && err == mgo.ErrNotFound {
+		avatar = Avatar{
+			UserID: user.UserID,
+			Avatar: "null",
+		}
+		err = c.Insert(avatar)
+		if err != nil {
+			return nil, err
+		}
+	} else if err != nil {
+		return nil, err
+	}
+	return &avatar, err
+}
+
+func (this *UserServiceProvider) ChangePassword(conn orm.Connection, id uint32, change *ChangePass) error {
+	var (
+		user User
+		err  error
+	)
+
+	tx := conn.(*gorm.DB).Begin()
+	defer func() {
+		if err != nil {
+			err = tx.Rollback().Error
+		} else {
+			err = tx.Commit().Error
+		}
+	}()
+
+	err = tx.Where("id = ?", id).First(&user).Error
+	if err == gorm.ErrRecordNotFound {
+		return err
+	}
+
+	if !security.SaltHashCompare([]byte(user.Password), &change.OldPass) {
+		err = ErrInvalidPass
+		return err
+	}
+
+	salt, err := security.SaltHashGenerate(&change.NewPass)
+	if err != nil {
+		return err
+	}
+
+	user.Password = string(salt)
+
+	err = tx.Save(&user).Error
+	return err
+}
+
+// GetUserByID gets user's information by userId.
+func (this *UserServiceProvider) GetUserByID(conn orm.Connection, userId uint32) (*User, error) {
 	db := conn.(*gorm.DB).Exec("USE user")
 	user := &User{}
 
-	err = db.Where("id = ?", userId).First(&user).Error
+	err := db.Where("id = ?", userId).First(&user).Error
 
 	return user, err
 }
